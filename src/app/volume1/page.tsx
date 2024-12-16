79c87c1503eb561505f983999d13fd90733f4f13
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2024-12-16 00:41:57 -0500

    better classing for list of poems

--- src/app/volume1/page.tsx
+++ src/app/volume1/page.tsx
@@ -7,18 +7,20 @@ export default async function Index() {
   const list = poems.map((poem) => {
     const link = `volume1/${poem.slug}`
     return (
-      <div key={poem.slug}>
+      <li key={poem.slug} className="mb-3 min-w-60">
         <Link href={link} className="hover:underline">
           <h2>{poem.author?.name}</h2>
           <h1>{poem.title}</h1>
         </Link>
-      </div>
+      </li>
     )
   })
   return (
     <div className="h-screen bg-slate-300">
       <Header />
-      <article className="grid place-items-center">{list}</article>
+      <article className="grid place-items-center">
+        <ul>{list}</ul>
+      </article>
     </div>
   )
 }
