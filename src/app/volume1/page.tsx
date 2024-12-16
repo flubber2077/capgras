b26a89c08d6aad51eb1d624d45ec9d9c57a5936c
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2024-12-16 13:12:12 -0500

    remove default images and refactor volume page

--- src/app/volume1/page.tsx
+++ src/app/volume1/page.tsx
@@ -1,33 +1,31 @@
 import Header from '@/components/Header'
 import Link from 'next/link'
+import { OstDocument } from 'outstatic'
 import { getDocuments } from 'outstatic/server'
 
 export default async function Index() {
   const poems = await getData()
-  const list = poems.map((poem) => {
-    const link = `volume1/${poem.slug}`
-    return (
-      <li
-        key={poem.slug}
-        className="w-full min-w-64 px-10 md:w-1/2"
-      >
-        <Link href={link} className="hover:underline">
-          <h3>{poem.author?.name}</h3>
-          <h2>{poem.title}</h2>
-        </Link>
-      </li>
-    )
-  })
   return (
     <div>
       <Header />
       <article className="prose grid max-w-none place-items-center prose-h1:mb-0 prose-h1:text-center prose-h3:mt-0">
         <h1>Volume 1</h1>
         <ul className="flex w-full max-w-7xl list-none flex-wrap justify-around p-0 after:flex-auto">
-          {list}
+          {poems.map(formatPoemInfoIntoLink)}
         </ul>
       </article>
     </div>
+  )
+}
+
+const formatPoemInfoIntoLink = (poem: OstDocument) => {
+  return (
+    <li key={poem.slug} className="my-0 w-full min-w-64 px-10 md:w-1/2">
+      <Link href={`volume1/${poem.slug}`} className="hover:underline">
+        <h3>{poem.author?.name}</h3>
+        <h2>{poem.title}</h2>
+      </Link>
+    </li>
   )
 }
 
