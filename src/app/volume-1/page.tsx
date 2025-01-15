423fc964f4f9f947e22fe61632fddd4904a60683
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2025-01-14 22:07:07 -0500

    reformat volume section

--- src/app/volume-1/page.tsx
+++ src/app/volume-1/page.tsx
@@ -6,7 +6,7 @@ export default async function Index() {
   return (
     <article className="prose grid max-w-none place-items-center prose-h1:mb-0 prose-h1:text-center prose-h3:mt-0">
       <h1>Volume 1</h1>
-      <ul className="flex w-full max-w-7xl list-none flex-wrap justify-around p-0 after:flex-auto">
+      <ul className="flex w-full max-w-7xl list-none flex-wrap justify-around p-0">
         {poems.sort(sortPoems).map(formatPoemInfoIntoLink)}
       </ul>
     </article>
@@ -26,14 +26,14 @@ function sortPoems(a: Poem, b: Poem) {
 function formatPoemInfoIntoLink(poem: Poem) {
   const { slug, frontmatter } = poem
   return (
-    <li key={slug} className="my-0 w-full min-w-64 lg:w-1/2">
+    <li key={slug} className="my-0 w-full min-w-64 md:w-1/2">
       <div className="">
         <Link
           href={`volume-1/${poem.slug}`}
-          className="flex min-h-20 justify-center no-underline hover:underline"
+          className="m-8 flex min-h-24 flex-col text-center no-underline hover:underline"
         >
-          <h3 className="flex-1 text-right">{`${frontmatter.firstName} ${frontmatter.lastName}`}</h3>
-          <h2 className="ml-3 flex-2">{frontmatter.title}</h2>
+          <h2 className="my-0">{frontmatter.title}</h2>
+          <h3 className="">{`${frontmatter.firstName} ${frontmatter.lastName}`}</h3>
         </Link>
       </div>
     </li>
