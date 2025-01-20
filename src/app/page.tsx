a4c03371f532491f7f2e2a056ec7637ed790f0b0
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2025-01-19 22:12:30 -0500

    reformat landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import { Navbar } from '@/components/Navbar'
 import Image from 'next/image'
 import TitlePageImage from '../../public/images/TitlePageImage.jpg'
+import Header from '@/components/Header'
 
 export default async function Index() {
   return (
-    <main className="prose:max-w-none prose:text-center prose mx-auto mt-10 list-none text-center text-xl backdrop-blur-xl prose-ul:list-none">
-      <h1 className="mb-20 font-header text-6xl font-medium text-slate-800 sm:text-8xl">
-        CAPGRAS
-      </h1>
-      <Navbar/>
-      <Image src={TitlePageImage} alt="pic of two people" placeholder='blur'/>
+    <main className="mx-auto">
+      <Header />
+      <Navbar />
+      <Image
+        src={TitlePageImage}
+        alt="pic of two people"
+        placeholder="blur"
+        className="mt-12"
+      />
     </main>
   )
 }
