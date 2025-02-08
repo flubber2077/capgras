bd09c375bf918daacc62ecedaed87b55673730da
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2025-02-07 23:06:16 -0500

    better html

--- src/app/volume-1/[slug]/page.tsx
+++ src/app/volume-1/[slug]/page.tsx
@@ -13,9 +13,11 @@ export default async function Poem({ params }: Params) {
   return (
     <article className="mt-32 w-full max-w-full">
       <section className="mx-auto max-w-4xl px-5">
-        <h1 className="">{fullName}</h1>
-        <h2 className="mb-4 italic">{title}</h2>
-        {subtitle ? <h3 className="mb-10 mt-0">{subtitle}</h3> : null}
+      <div className="flex flex-col">
+          <h1 className="order-2 mb-4 italic">{title}</h1>
+          <h2 className="order-1">{fullName}</h2>
+          {subtitle ? <h3 className="order-3 mb-10 mt-0">{subtitle}</h3> : null}
+        </div>
         {content}
         <hr className="mx-auto mt-48 h-0.5 max-w-xl" />
         <p
