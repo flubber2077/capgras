14110c8131748813779215212531f8e4a6357f37
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2025-11-17 20:29:52 -0500

    get redirect to work

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        destination: '/volumes/1/:slug',
+        permanent: true,
+        source: '/volume-1/:slug',
+      },
+    ];
+  },
   transpilePackages: ['next-mdx-remote'],
 };
 
-module.exports = nextConfig;
+export default nextConfig;
