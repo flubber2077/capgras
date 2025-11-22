160ff26c0aa18600826c2521b5af62c9141e52e4
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2025-11-22 12:59:19 -0500

    fix linting

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -6,7 +6,7 @@ export const dynamic = 'force-static';
 export async function GET() {
   const data = await getDataOfAllVolumes();
 
-  const rss = await generateRss(data);
+  const rss = generateRss(data);
 
   return new Response(rss.rss2(), {
     headers: { 'Content-Type': 'application/xml; charset=utf-8' },
