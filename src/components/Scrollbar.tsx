22c64c6b62fbf7df09231a97c536c01d9daf2e5d
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2026-06-14 21:43:48 -0400

    fix linting issue

--- src/components/Scrollbar.tsx
+++ src/components/Scrollbar.tsx
@@ -4,20 +4,16 @@ import * as motion from 'motion/react-client';
 
 export default function Scrollbar() {
   const { scrollYProgress } = useScroll();
-  return (
-    <motion.div
-      // eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
-      style={{
-        scaleX: scrollYProgress,
-        height: 5,
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        originX: 0,
-        position: 'fixed',
-        backgroundColor: '#447',
-      }}
-    />
-  );
+  const scrollbarStyle = {
+    scaleX: scrollYProgress,
+    height: 5,
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    originX: 0,
+    position: 'fixed',
+    backgroundColor: '#447',
+  } as const;
+  return <motion.div style={scrollbarStyle} />;
 }
